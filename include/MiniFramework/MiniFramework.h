aa02fb888dfcea6de8347d4b497fd7c80e59ba03
minhyuk <minhyuk@live.co.kr>
2024-04-25 10:54:57 +0900

    Veronica: Docstring Generator

--- include/MiniFramework/MiniFramework.h
+++ include/MiniFramework/MiniFramework.h
@@ -8,6 +8,11 @@ template <typename V>
 class IInputProcessor
 {
 public:
+    /**
+     * A pure virtual function intended to process the input of type V.
+     * This function must be implemented by derived classes to handle specific input processing.
+     * @param obj The input object of type V that needs to be processed.
+     */
     virtual void process(const V& obj) = 0;
 };
 
@@ -16,18 +21,31 @@ template <typename T>
 class Convey
 {
 public:
+    /**
+     * Virtual destructor for the Convey class.
+     * Ensures derived classes can clean up properly.
+     */
     virtual ~Convey(void){}
 
+    /**
+     * Adds a new input processor to the convey mechanism.
+     * This allows the convey to forward the goods to the next processor in the workflow.
+     * @param inputProcessor A pointer to the input processor to be added to the convey mechanism.
+     */
     void addNextProcessor(IInputProcessor<T> * inputProcessor ){
         inputProcessors_.push_back(inputProcessor);
     }
 
-    
+    /**
+     * Propagates the given good through all added input processors.
+     * Each input processor processes the good in the order they were added.
+     * @param good The good to be propagated through the input processors.
+     */
     void propagate(const T& good){
-    	typename std::vector<IInputProcessor<T> *>::iterator i;
-    	for(i = inputProcessors_.begin(); i!=inputProcessors_.end();++i){
-    		(*i)->process(good);
-    	}
+        typename std::vector<IInputProcessor<T> *>::iterator i;
+        for(i = inputProcessors_.begin(); i!=inputProcessors_.end();++i){
+            (*i)->process(good);
+        }
     }
 
 private:
@@ -35,9 +53,11 @@ private:
 };
 
 /**
- * one input and one output worker
+ * Represents a generic worker that can handle one type of input and produce another type of output.
+ * Inherits from both IInputProcessor to process input and Convey to handle output propagation.
  */
 template <typename From, typename To>
 class Worker : public IInputProcessor<From>, public Convey<To>{};
 
 #endif // __MiniFramework_h__
+
