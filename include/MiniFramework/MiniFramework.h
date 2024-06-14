f4262ec29eda8e72604bc0a33dd36daff025fc75
minhyuk <minhyuk@live.co.kr>
2024-06-14 13:29:42 +0900

    [Veronica] Function Comment(Korean)

--- include/MiniFramework/MiniFramework.h
+++ include/MiniFramework/MiniFramework.h
@@ -33,8 +33,8 @@ public:
      * @param inputProcessor A pointer to the input processor to be added to the convey mechanism.
      */
     void addNextProcessor(IInputProcessor<T> * inputProcessor ){
-        inputProcessors_.push_back(inputProcessor);
-    }
+            inputProcessors_.push_back(inputProcessor);
+        }
 
     /**
      * Propagates the given good through all added input processors.
@@ -42,11 +42,11 @@ public:
      * @param good The good to be propagated through the input processors.
      */
     void propagate(const T& good){
-        typename std::vector<IInputProcessor<T> *>::iterator i;
-        for(i = inputProcessors_.begin(); i!=inputProcessors_.end();++i){
-            (*i)->process(good);
+            typename std::vector<IInputProcessor<T> *>::iterator i;
+            for(i = inputProcessors_.begin(); i!=inputProcessors_.end();++i){
+                (*i)->process(good);
+            }
         }
-    }
 
 private:
     std::vector<IInputProcessor<T> *> inputProcessors_;
